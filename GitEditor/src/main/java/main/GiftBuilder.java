4f5e77e8ab4087cccd789f0401a29dd5e5dc311b
Alan Stefan <G00247318@gmit.ie>
2012-12-04 18:19:52 +0000

    True/False GUI completed, added Feel&Look for GUI with cross platForm

--- GitEditor/src/main/java/main/GiftBuilder.java
+++ GitEditor/src/main/java/main/GiftBuilder.java
@@ -1,5 +1,5 @@
 package main;
 
-public class GiftBuilder {
+public class GiftBuilder implements DataAccess{
 
 }
--- GitEditor/src/main/java/main/Main.java
+++ GitEditor/src/main/java/main/Main.java
@@ -1,5 +1,9 @@
 package main;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
+
 public class Main {
 
 	/**
@@ -7,6 +11,21 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		GUI app=new GUI();
 	}
 
